b77925507706d1ffdcf905e967743ad136c40da3
yogirajh007 <yogirajhendre5@gmail.com>
2021-05-12 19:26:36 +0530

    Some changes fixed

--- js/functions.js
+++ js/functions.js
@@ -4,8 +4,8 @@
 */
 let bandcampLinks = document.getElementsByClassName('bandcamp-link');
 
-for( var i = 0; i < bandcampLinks.length; i++ ){
-	bandcampLinks[i].addEventListener('click', function(e){
+for (var i = 0; i < bandcampLinks.length; i++) {
+	bandcampLinks[i].addEventListener('click', function (e) {
 		e.stopPropagation();
 	});
 }
@@ -13,17 +13,17 @@ for( var i = 0; i < bandcampLinks.length; i++ ){
 
 songElements = document.getElementsByClassName('song');
 
-for( var i = 0; i < songElements.length; i++ ){
+for (var i = 0; i < songElements.length; i++) {
 	/*
 		Ensure that on mouseover, CSS styles don't get messed up for active songs.
 	*/
-	songElements[i].addEventListener('mouseover', function(){
+	songElements[i].addEventListener('mouseover', function () {
 		this.style.backgroundColor = '#00A0FF';
 
 		this.querySelectorAll('.song-meta-data .song-title')[0].style.color = '#FFFFFF';
 		this.querySelectorAll('.song-meta-data .song-artist')[0].style.color = '#FFFFFF';
 
-		if( !this.classList.contains('amplitude-active-song-container') ){
+		if (!this.classList.contains('amplitude-active-song-container')) {
 			this.querySelectorAll('.play-button-container')[0].style.display = 'block';
 		}
 
@@ -35,7 +35,7 @@ for( var i = 0; i < songElements.length; i++ ){
 	/*
 		Ensure that on mouseout, CSS styles don't get messed up for active songs.
 	*/
-	songElements[i].addEventListener('mouseout', function(){
+	songElements[i].addEventListener('mouseout', function () {
 		this.style.backgroundColor = '#FFFFFF';
 		this.querySelectorAll('.song-meta-data .song-title')[0].style.color = '#272726';
 		this.querySelectorAll('.song-meta-data .song-artist')[0].style.color = '#607D8B';
@@ -48,7 +48,7 @@ for( var i = 0; i < songElements.length; i++ ){
 	/*
 		Show and hide the play button container on the song when the song is clicked.
 	*/
-	songElements[i].addEventListener('click', function(){
+	songElements[i].addEventListener('click', function () {
 		this.querySelectorAll('.play-button-container')[0].style.display = 'none';
 	});
 
@@ -59,67 +59,43 @@ for( var i = 0; i < songElements.length; i++ ){
 	Initializes AmplitudeJS
 */
 
-$("#search").on('keypress',function (e) {
+$("#search").on('keypress', function (e) {
 	if (e.keyCode == 13) {
 		searchkey = document.getElementById("search").value;
+		if (Amplitude.getPlayerState() == "playing") {
+			Amplitude.stop();
+		}
 		$('#blue-playlist-container').hide();
-		$.get('https://api.music.dexteroot.ml/result/', {query: searchkey}, function (data) {
+		$.get('https://api.music.dexteroot.ml/result/', {
+			query: searchkey
+		}, function (data) {
 			console.log(data);
 			datum = data;
-		$('#blue-playlist-container').show()
-		
-		
-		
+			$('#blue-playlist-container').show()
+
+			mysongs = [];
+			datum.forEach(element => {
+				var n = {
+					name: element['song'],
+					artist: element['primary_artist'],
+					album: element['album'],
+					url: element['media_url'],
+					cover_art_url: element['image']
+				}
+				mysongs.push(n);
+			});
+
+
 			Amplitude.init({
 				continue_next: true,
-				
-			
-			
-				"songs": [
-					{
-						"name": datum[0]['song'],
-						"artist": datum[0]['primary_artists'],
-						"album": datum[0]['album'],
-						"url": datum[0]['media_url'],
-						"cover_art_url": datum[0]['image']
-					},
-					{
-						"name": datum[1]['song'],
-						"artist": datum[1]['primary_artists'],
-						"album": datum[0]['album'],
-						"url": datum[1]['media_url'],
-						"cover_art_url": datum[1]['image']
-					},
-					{
-						"name": datum[2]['song'],
-						"artist": datum[2]['primary_artists'],
-						"album": datum[2]['album'],
-						"url": datum[2]['media_url'],
-						"cover_art_url": datum[2]['image']
-					},
-					{
-						"name": datum[3]['song'],
-						"artist": datum[3]['primary_artists'],
-						"album": datum[3]['album'],
-						"url": datum[3]['media_url'],
-						"cover_art_url": datum[3]['image']
-					},
-					{
-						"name": datum[4]['song'],
-						"artist": datum[4]['primary_artists'],
-						"album": datum[4]['album'],
-						"url": datum[4]['media_url'],
-						"cover_art_url": datum[4]['image']
-					}
-				]
+				"songs": mysongs
 			});
-			for( var i = 0; i < songElements.length; i++ )
-			{
+			for (var i = 0; i < songElements.length; i++) {
 				songElements[i].querySelectorAll('.song-meta-data .song-title')[0].innerHTML = datum[i]['song'];
 				songElements[i].querySelectorAll('.song-meta-data .song-artist')[0].innerHTML = datum[i]['primary_artists'];
-			}	
-		
+			}
+
 		});
-			
+
 	}
- });
+});
